ae2252152c603448c6427c3041513d04d2b684ce
֎ Yasir Eymen Kayabaşı <93262823+yaso09@users.noreply.github.com>
2022-10-08 12:34:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-import "./classicTerminal.css";
-import "./terminal1.ts";
 /*
     Terminal.JS created by Yasir Eymen Kayabaşı.
 */
+import "./classicTerminal.css";
+import "./terminal1.ts";
+
+terminal.screen.clear();
+
+terminal.screen.write("Terminal.js embedded.");
+console.log("Terminal.js embedded.");
